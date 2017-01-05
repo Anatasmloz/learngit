ac598d9dba33dfa53b4cecf0070367b4e59164e6
Anastasmloza <anatasmloz@foxmail.com>
2017-01-05 20:17:52 +0800

    core change

--- jia.cpp
+++ jia.cpp
@@ -1,41 +1,34 @@
-#include<iostream>
-#include<vector>
+#include<bits/stdc++.h>
+#define pb push_back
+#define FOR(i, n) for (int i = 0; i < (int)n; ++i)
+#define dbg(x) cout << #x << " at line " << __LINE__ << " is: " << x << endl
+typedef long long ll;
 using namespace std;
-
-const int MAXN = 10005;
-long long int dp[MAXN][MAXN];
-vector<int> ori;
-int main()
-{
+typedef pair<int, int> pii;
+const int maxn = 1e3 + 10;
+int n, m;
+long long dp[maxn][maxn];
+int a[maxn];
+void solve() {
 	freopen("input.txt", "r", stdin);
-	int N, M;
-	cin >> N >> M;
-	ori.resize(N + 1);
-	for (int i = 1; i <= N; i ++)
-	{
-		cin >> ori[i];
-	}
-	dp[0][0] = 1;
-	for (int i = 1; i <= N; i ++)
-	{
-		for (int j = M; j >= 0; j --)
-		{
-			if(j >= ori[i])
-			dp[i][j] = dp[i - 1][j - ori[i]] + dp[i - 1][j];
-			else
-			dp[i][j] = dp[i - 1][j];
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) cin >> a[i];
+   for (int i = 0; i <= maxn; i ++)
+   dp[i][0] = 1;
+    for (int i = 1; i <= n; i++) {
+        for (int j = m; j >= a[i]; j--) {
+            dp[i][j] = dp[i - 1][j - a[i]] + dp[i - 1][j];
+        }
+        for (int j = a[i] - 1; j >= 0; j --)
+        {
+        	dp[i][j] = dp[i - 1][j];
 		}
-	}
-	cout << dp[N][M] << endl;
-	return 0;
+    }
+    cout << dp[n][m] << endl;
 }
-
-
-
-
-
-
-
-
-
-
+int main() {
+    //freopen("test.in", "r", stdin);
+    //freopen("test.out", "w", stdout);
+    solve();
+    return 0;
+}
--- readme.txt
+++ readme.txt
@@ -1,4 +1,4 @@
 Git is a version control system.
 Git is free software available under GPL.
 The first change.
-Git tracks changes.
+Git tracks changes of files.
